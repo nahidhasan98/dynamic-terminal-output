808e4c58e98065ba874c7ae992a94643ba83ba4d
Nahid Hasan <mnh.nahid35@gmail.com>
2022-08-23 11:57:09 +0600

    circular printing

---
+++ anim.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+height=8
+width=10
+row=0
+col=0
+
+while [ $row -lt $height ] && [ $col -lt $width ]; do
+    # up side
+    for ((i=$col;i<$width;i++)); do
+        printf "*"
+        sleep 0.05
+    done
+    ((row+=1))
+
+    # right side
+    for ((i=$row;i<$height;i++)); do
+        echo -en "\033[1B\033[1D"
+        printf "*"
+        sleep 0.05
+    done
+    ((col+=1))
+
+    # down side
+    for ((i=$col;i<$width;i++)); do
+        echo -en "\033[2D"
+        printf "*"
+        sleep 0.05
+    done
+    ((row+=1))
+
+    # left side
+    for ((i=$row;i<$height;i++)); do
+        echo -en "\033[1A\033[1D"
+        printf "*"
+        sleep 0.05
+    done
+    ((col+=1))
+done
+
+echo -en "\033[$(($height/2))B\033[$(($width/2))C"
+printf "\n"
